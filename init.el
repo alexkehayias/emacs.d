98cd7f7a9b3011ad600dd84e617c2dc518b208c1
Alex Kehayias <alex.kehayias@gmail.com>
2020-01-11 10:51:16 -0800

    Add gitignore

--- init.el
+++ init.el
@@ -782,6 +782,7 @@
 (use-package projectile
   :ensure t
   :config
+  (projectile-mode)
   ;; Set the base keybinding
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   ;; Cache projectile projects
