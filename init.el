e750af0c9959e4c466d4c27c6e7544b2c660ef9c
Alex Kehayias <alex.kehayias@gmail.com>
2020-08-12 13:29:55 -0700

    Disable global flycheck mode

--- init.el
+++ init.el
@@ -111,7 +111,7 @@
 (use-package flycheck
   :ensure t
   :config
-  (global-flycheck-mode)
+  ;; (global-flycheck-mode)
   (setq flycheck-global-modes '(not rust-mode))
   (global-set-key (kbd "C-c M-n") 'flycheck-next-error)
   (global-set-key (kbd "C-c M-p") 'flycheck-previous-error))
