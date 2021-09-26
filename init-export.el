3ea9b9acbcce21b9289a7cc424365db5017e486a
Alex Kehayias <alex.kehayias@gmail.com>
2021-09-26 08:26:39 -0700

    Remove workaround for builtin org mode

--- init-export.el
+++ init-export.el
@@ -130,24 +130,6 @@
 
 ;; Org-mode
 
-;; Copied from https://github.com/glasserc/etc/commit/3af96f2c780a35d35bdf1b9ac19d80fe2e6ebbf8
-;; Work around built-in org-mode so we can load from ELPA.
-;; First, remove the built-in org directory from the load-path.
-;; Thanks to
-;; http://stackoverflow.com/questions/20603578/emacs-does-not-see-new-installation-of-org-mode/20616703#20616703.
-;; Without this, use-package will try to require org and succeed.
-(eval-when-compile
-  (require 'cl))
-
-(setq load-path (remove-if (lambda (x) (string-match-p "org$" x)) load-path))
-;; Second, trick emacs into forgetting about the fact that org is
-;; a "built-in" package by removing it from package--builtins.
-;; Without this, package will refuse to install org, since it's
-;; "already installed".
-;; package--builtins is only initialized when a query needs it.
-(package-built-in-p 'org)   ;; prime package--builtins
-(setq package--builtins (assq-delete-all 'org package--builtins))
-
 (use-package org
   :config
   (setq org-directory "~/Org")
@@ -738,6 +720,10 @@
 
     (buffer-string)))
 
+;; Needed for functions like `first`
+(eval-when-compile
+  (require 'cl))
+
 ;; Org roam
 ;; These are specified so they can be dynamically configured
 ;; by calling emacs in batch mode in a CI context
@@ -917,5 +903,7 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
   (add-hook 'json-mode 'flymake-json-load))
+
+(straight-thaw-versions)
 
 (provide 'export)
