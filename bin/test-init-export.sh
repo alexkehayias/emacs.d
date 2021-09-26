6724d1bc80c5388a628d830ef9a0f84ffb836d74
Alex Kehayias <alex@mosey.so>
2021-09-26 14:41:53 -0700

    Test both init and init-export

--- bin/test-init-export.sh
+++ bin/test-init-export.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
-echo "Attempting startup..."
 # This is required by org-roam or it will fail to init
 mkdir -p ~/Org/notes
+# Load the init-export and install packages
+# Note: there is no need to `straight-thaw-versions` since that is
+# part of init-export
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
                               (url-show-status nil)
                               (user-emacs-directory default-directory)
-                              (user-init-file (expand-file-name "init.el"))
+                              (user-init-file (expand-file-name "init-export.el"))
                               (load-path (delq default-directory load-path)))
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook)))'
-echo "Startup successful"
---
+++ bin/test-init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+# This is required by org-roam or it will fail to init
+mkdir -p ~/Org/notes
+# Load the init file and install packages
+${EMACS:=emacs} -nw --batch \
+                --eval '(let ((debug-on-error t)
+                              (url-show-status nil)
+                              (user-emacs-directory default-directory)
+                              (user-init-file (expand-file-name "init.el"))
+                              (load-path (delq default-directory load-path)))
+                           (load-file user-init-file)
+                           (run-hooks (quote after-init-hook))
+                           (straight-thaw-versions))'
