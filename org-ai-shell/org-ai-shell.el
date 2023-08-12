af4edede706bd08ace4795770bf98908e0d8d87f
Alex Kehayias <alex@mosey.so>
2023-08-12 09:47:11 -0700

    Add shell around org-ai

---
+++ org-ai-shell/org-ai-shell.el
@@ -0,0 +1,58 @@
+;; Provides a wrapper for the org-ai chat interface using comint.
+;; Based off of the tutorial here
+;; https://www.masteringemacs.org/article/comint-writing-command-interpreter
+
+(defvar my-org-ai-process nil
+  "Variable to store the org-ai process.")
+
+(defvar org-ai-cli-file-path "~/Projects/org-ai/src/chat.py"
+  "Path to the program used by `org-ai'")
+
+(defvar org-ai-cli-arguments '()
+  "Commandline arguments to pass to `org-ai'.")
+
+(defvar org-ai-mode-map
+  (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
+    ;; example definition
+    (define-key map "\t" 'completion-at-point)
+    map)
+  "Basic mode map for `org-ai'.")
+
+(defvar org-ai-prompt-regexp "^\\(?:\\[[^@]+@[^@]+\\]\\)"
+  "Prompt for `org-ai'.")
+
+(defvar org-ai-buffer-name "*Org AI*"
+  "Name of the buffer to use for the `org-ai' comint instance.")
+
+(defun org-ai ()
+  "Run an inferior instance of `org-ai-cli' inside Emacs."
+  (interactive)
+  (let* ((org-ai-program org-ai-cli-file-path)
+         (buffer (get-buffer-create org-ai-buffer-name))
+         (proc-alive (comint-check-proc buffer))
+         (process (get-buffer-process buffer)))
+    ;; if the process is dead then re-create the process and reset the
+    ;; mode.
+    (unless proc-alive
+      (with-current-buffer buffer
+        (apply 'make-comint-in-buffer "Org-Ai" buffer
+               org-ai-program nil org-ai-cli-arguments)
+        (org-ai-mode)))
+    ;; Regardless, provided we have a valid buffer, we pop to it.
+    (when buffer
+      (pop-to-buffer buffer))))
+
+(defun org-ai--initialize ()
+  "Helper function to initialize Org-Ai."
+  ;; TODO: Set special comint settings here
+  )
+
+(define-derived-mode org-ai-mode comint-mode "Org AI"
+  "Major mode for `org-ai'.
+
+\\<org-ai-mode-map>"
+  (setq comint-prompt-read-only t)
+  ;; this makes it so commands like M-{ and M-} work.
+  (set (make-local-variable 'paragraph-separate) "\\'"))
+
+(add-hook 'org-ai-mode-hook 'org-ai--initialize)
